fb669de55d087c4d5875b17a797ec0a302203772
kadeshar <kadeshar@gmail.com>
2025-01-08 18:06:03 +0100

    - changelog for 0.3

--- template/icecrown/tpl/changelog.php
+++ template/icecrown/tpl/changelog.php
@@ -2,16 +2,30 @@
 ?>
 <div class="content_box1" style="line-height: 1.5;">
     <h1>Changelog</h1>
+
+    <div>
+        <strong>Version 0.3</strong> - 
+        <em>2025-01-08</em>
+        <ul>
+            <li>Updated AzerothCore source <a href="https://github.com/kadeshar/azerothcore-wotlk/compare/repack-0.2...repack-0.3" target="_blank">Source changelog</a></li>
+            <li>Updated Playerbot source <a href="https://github.com/kadeshar/mod-playerbots/compare/repack-0.2...repack-0.3" target="_blank">Source changelog</a></li>
+            <li>[Addons] Added WoWSimExporter addon</li>
+            <li>[Addons] Changed addon AzerothCoreAdmin on AzerothAdmin (more updated source)</li>
+            <li>[Trion Control Panel] Fixed error while first save new ip address</li>
+            <li>[Trion Control Panel] Added prompt before closing database while world/logon server running</li>
+            <li>[Trion Control Panel] Blocked unsupported features</li>
+        </ul>
+    </div>
     
     <div>
         <strong>Version 0.2</strong> - 
         <em>2025-01-01</em>
         <ul>
-            <li>Updated AzerothCore source <a href="https://github.com/kadeshar/azerothcore-wotlk/compare/release/0.1...kadeshar:azerothcore-wotlk:Playerbot" target="_blank">Source changelog</a></li>
-            <li>Updated Playerbot source <a href="https://github.com/kadeshar/azerothcore-wotlk/compare/release/0.1...kadeshar:azerothcore-wotlk:Playerbot" target="_blank">Source changelog</a></li>
-            <li>(Trion Control Panel) Fixed visual bug with refreshing values after saving database changes</li>
-            <li>(Website) Added changelog</li>
-            <li>(Installation) Added required runtime libraries for Windows 10</li>
+            <li>Updated AzerothCore source <a href="https://github.com/kadeshar/azerothcore-wotlk/compare/release/0.1...kadeshar:azerothcore-wotlk:release/0.2" target="_blank">Source changelog</a></li>
+            <li>Updated Playerbot source <a href="https://github.com/kadeshar/mod-playerbots/compare/release/0.1...kadeshar:mod-playerbots:release/0.2" target="_blank">Source changelog</a></li>
+            <li>[Trion Control Panel] Fixed visual bug with refreshing values after saving database changes</li>
+            <li>[Website] Added changelog</li>
+            <li>[Installation] Added required runtime libraries for Windows 10</li>
         </ul>
     </div>
     
